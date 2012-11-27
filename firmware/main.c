c0a7d620515428c70e135d022705d0a4ca51d3a0
Mathias Dalheimer <md@gonium.net>
2012-11-27 16:39:50 +0100

    added adc routines

--- firmware/main.c
+++ firmware/main.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <avr/io.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 
 #include "uart.h"
+#include "adc.h"
 
 
 /* define CPU frequency in Mhz here if not defined in Makefile */
@@ -14,110 +16,47 @@
 /* 9600 baud */
 #define UART_BAUD_RATE      9600      
 
+void init(void) {
+  /*
+   *  Initialize UART library, pass baudrate and AVR cpu clock
+   *  with the macro 
+   *  UART_BAUD_SELECT() (normal speed mode )
+   *  or 
+   *  UART_BAUD_SELECT_DOUBLE_SPEED() ( double speed mode)
+   */
+  uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) ); 
+  adc_init();
+
+  /*
+   * now enable interrupt, since UART library is interrupt controlled
+   */
+  sei();
+}
 
 int main(void)
 {
-    unsigned int c;
-    char buffer[7];
-    int  num=134;
+  char buffer[7];
+  uint16_t num=0;
 
-    
-    /*
-     *  Initialize UART library, pass baudrate and AVR cpu clock
-     *  with the macro 
-     *  UART_BAUD_SELECT() (normal speed mode )
-     *  or 
-     *  UART_BAUD_SELECT_DOUBLE_SPEED() ( double speed mode)
-     */
-    uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) ); 
-    
-    /*
-     * now enable interrupt, since UART library is interrupt controlled
-     */
-    sei();
-    
-    /*
-     *  Transmit string to UART
-     *  The string is buffered by the uart library in a circular buffer
-     *  and one character at a time is transmitted to the UART using interrupts.
-     *  uart_puts() blocks if it can not write the whole string to the circular 
-     *  buffer
-     */
-    uart_puts("String stored in SRAM\n\r");
-    
-    /*
-     * Transmit string from program memory to UART
-     */
-    uart_puts_P("String stored in FLASH\n\r");
-    
-        
-    /* 
-     * Use standard avr-libc functions to convert numbers into string
-     * before transmitting via UART
-     */     
+  init();
+
+  /*
+   * Transmit string from program memory to UART
+   */
+  uart_puts_P("HexaSense prototype\n\r");
+
+
+  /* 
+   * Use standard avr-libc functions to convert numbers into string
+   * before transmitting via UART
+   */     
+
+  while(1) {
+    num = adc_get_single_sample(0);
     itoa( num, buffer, 10);   // convert interger into string (decimal format)         
     uart_puts(buffer);        // and transmit string to UART
+    uart_puts_P("\r\n");
+    _delay_ms(1000);
+  }
 
-    
-    /*
-     * Transmit single character to UART
-     */
-    uart_puts("\n\r");
-    
-    for(;;)
-    {
-        /*
-         * Get received character from ringbuffer
-         * uart_getc() returns in the lower byte the received character and 
-         * in the higher byte (bitmask) the last receive error
-         * UART_NO_DATA is returned when no data is available.
-         *
-         */
-        c = uart_getc();
-        if ( c & UART_NO_DATA )
-        {
-            /* 
-             * no data available from UART 
-             */
-        }
-        else
-        {
-            /*
-             * new data available from UART
-             * check for Frame or Overrun error
-             */
-            if ( c & UART_FRAME_ERROR )
-            {
-                /* Framing Error detected, i.e no stop bit detected */
-                uart_puts_P("UART Frame Error: ");
-            }
-            if ( c & UART_OVERRUN_ERROR )
-            {
-                /* 
-                 * Overrun, a character already present in the UART UDR register was 
-                 * not read by the interrupt handler before the next character arrived,
-                 * one or more received characters have been dropped
-                 */
-                uart_puts_P("UART Overrun Error: ");
-            }
-            if ( c & UART_BUFFER_OVERFLOW )
-            {
-                /* 
-                 * We are not reading the receive buffer fast enough,
-                 * one or more received character have been dropped 
-                 */
-                uart_puts_P("Buffer overflow error: ");
-            }
-            /* 
-             * send received character back
-             */
-//            if ((unsigned char)c == '\n')
-//              uart_puts("\\n");
-//            else if ((unsigned char)c == '\r')
-//              uart_puts("\\r");
-//            else
-              uart_putc( (unsigned char)c );
-        }
-    }
-    
 }
