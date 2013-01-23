f21e0d415b68b892c66e117e54f1c4a02513eb56
Dominik Keller <kellerd@itwm.fhg.de>
2013-01-23 19:14:25 +0100

    added empty routine for conversion from adc value to temperature

---
+++ firmware/conversion.h
@@ -0,0 +1,10 @@
+/**
+ * convert adc value to temperature
+ */
+uint8_t temperature_conversion(uint16_t adc);
+
+#define TEMP_MIN -10
+#define TEMP_MAX  100
+#define ADC_MIN   0
+#define ADC_MAX   100
+
