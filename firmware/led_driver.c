8ab4d1f16ae6b0d45788c3913843af781458285a
Mathias Dalheimer <md@gonium.net>
2013-02-05 16:27:31 +0100

    added driver for hygrosens HYT271 humidity sensor

--- firmware/led_driver.c
+++ firmware/led_driver.c
@@ -32,30 +32,30 @@ uint8_t set_pca9532(
 	uint8_t ls2,
 	uint8_t ls3)
 {
-   i2c_init();
+  //i2c_init();
   // start communication with PCA9535
   if (i2c_start(PCA9532_ADDR+I2C_WRITE)) {
-	uart_puts_P("I2C: Failed to access device.\r\n");
-	return I2C_ERROR;
+    uart_puts_P("I2C: Failed to access device.\r\n");
+    return I2C_ERROR;
   } else {
-	// PSC0 subaddress + auto-increment
-	i2c_write(0x12);
-	// prescaler 0
-	i2c_write(psc0);
-	// pwm0 duty cycle
-	i2c_write(pwm0);
-	// prescaler 1
-	i2c_write(psc1);
-	// pwm1 duty cycle
-	i2c_write(pwm1);
-	// LED 0-3 
-	i2c_write(ls0);
-	// LED 4-7 
-	i2c_write(ls1);
-	// LED 8-11
-	i2c_write(ls2);
-	// LED 12-15 
-	i2c_write(ls3);
+    // PSC0 subaddress + auto-increment
+    i2c_write(0x12);
+    // prescaler 0
+    i2c_write(psc0);
+    // pwm0 duty cycle
+    i2c_write(pwm0);
+    // prescaler 1
+    i2c_write(psc1);
+    // pwm1 duty cycle
+    i2c_write(pwm1);
+    // LED 0-3 
+    i2c_write(ls0);
+    // LED 4-7 
+    i2c_write(ls1);
+    // LED 8-11
+    i2c_write(ls2);
+    // LED 12-15 
+    i2c_write(ls3);
   }
   i2c_stop();
   return I2C_SUCCESS;
@@ -64,33 +64,33 @@ uint8_t set_pca9532(
 
 void led_all_off(void) {
   if (set_pca9532(
-		0x00, // prescaler 0: maximum frequency
-		0xFF, // pwm0: 256/256
-		0x00, // prescaler 1: maximum frequency
-		0xFF, // pwm1: 256/256
-		PCA9532_OFF, // LED0-3: off.
-		PCA9532_OFF, // LED4-7: off.
-		PCA9532_OFF, // LED8-11: off.
-		PCA9532_OFF  // LED12-15: off.
-		)) 
+        0x00, // prescaler 0: maximum frequency
+        0xFF, // pwm0: 256/256
+        0x00, // prescaler 1: maximum frequency
+        0xFF, // pwm1: 256/256
+        PCA9532_OFF, // LED0-3: off.
+        PCA9532_OFF, // LED4-7: off.
+        PCA9532_OFF, // LED8-11: off.
+        PCA9532_OFF  // LED12-15: off.
+        )) 
   {
-	uart_puts_P("LED initialization failed.\r\n");
+    uart_puts_P("LED initialization failed.\r\n");
   }
 }
 
 void led_all_full(void) {
   if (set_pca9532(
-		0x00, // prescaler 0: maximum frequency
-		0xFF, // pwm0: 256/256
-		0x00, // prescaler 1: maximum frequency
-		0xFF, // pwm1: 256/256
-		PCA9532_FULL, // LED0-3: on.
-		PCA9532_FULL, // LED4-7: on.
-		PCA9532_FULL, // LED8-11: on.
-		PCA9532_FULL  // LED12-15: on.
-		)) 
+        0x00, // prescaler 0: maximum frequency
+        0xFF, // pwm0: 256/256
+        0x00, // prescaler 1: maximum frequency
+        0xFF, // pwm1: 256/256
+        PCA9532_FULL, // LED0-3: on.
+        PCA9532_FULL, // LED4-7: on.
+        PCA9532_FULL, // LED8-11: on.
+        PCA9532_FULL  // LED12-15: on.
+        )) 
   {
-	uart_puts_P("LED initialization failed.\r\n");
+    uart_puts_P("LED initialization failed.\r\n");
   }
 }
 
@@ -108,49 +108,49 @@ void led_bad(void) {
 
 void led_red_full(void) {
   if (set_pca9532(
-		0x00, // prescaler 0: maximum frequency
-		0xFF, // pwm0: 256/256
-		0x00, // prescaler 1: maximum frequency
-		0xFF, // pwm1: 256/256
-		PCA9532_FULL, // LED0-3: on.
-		PCA9532_OFF, // LED4-7: off.
-		PCA9532_OFF, // LED8-11: off.
-		PCA9532_OFF  // LED12-15: off.
-		)) 
+        0x00, // prescaler 0: maximum frequency
+        0xFF, // pwm0: 256/256
+        0x00, // prescaler 1: maximum frequency
+        0xFF, // pwm1: 256/256
+        PCA9532_FULL, // LED0-3: on.
+        PCA9532_OFF, // LED4-7: off.
+        PCA9532_OFF, // LED8-11: off.
+        PCA9532_OFF  // LED12-15: off.
+        )) 
   {
-	uart_puts_P("LED initialization failed.\r\n");
+    uart_puts_P("LED initialization failed.\r\n");
   }
 }
 
 void led_green_full(void) {
   if (set_pca9532(
-		0x00, // prescaler 0: maximum frequency
-		0xFF, // pwm0: 256/256
-		0x00, // prescaler 1: maximum frequency
-		0xFF, // pwm1: 256/256
-		PCA9532_OFF,
-		PCA9532_FULL,
-		PCA9532_OFF,
-		PCA9532_OFF
-		)) 
+        0x00, // prescaler 0: maximum frequency
+        0xFF, // pwm0: 256/256
+        0x00, // prescaler 1: maximum frequency
+        0xFF, // pwm1: 256/256
+        PCA9532_OFF,
+        PCA9532_FULL,
+        PCA9532_OFF,
+        PCA9532_OFF
+        )) 
   {
-	uart_puts_P("LED initialization failed.\r\n");
+    uart_puts_P("LED initialization failed.\r\n");
   }
 }
 
 void led_blue_full(void) {
   if (set_pca9532(
-		0x00, // prescaler 0: maximum frequency
-		0xFF, // pwm0: 256/256
-		0x00, // prescaler 1: maximum frequency
-		0xFF, // pwm1: 256/256
-		PCA9532_OFF,
-		PCA9532_OFF,
-		PCA9532_FULL,
-		PCA9532_OFF
-		)) 
+        0x00, // prescaler 0: maximum frequency
+        0xFF, // pwm0: 256/256
+        0x00, // prescaler 1: maximum frequency
+        0xFF, // pwm1: 256/256
+        PCA9532_OFF,
+        PCA9532_OFF,
+        PCA9532_FULL,
+        PCA9532_OFF
+        )) 
   {
-	uart_puts_P("LED initialization failed.\r\n");
+    uart_puts_P("LED initialization failed.\r\n");
   }
 }
 
--- firmware/main.c
+++ firmware/main.c
@@ -8,9 +8,11 @@
 #include "uart.h"
 #include "adc.h"
 #include "dht_sensor.h"
+#include "hyt271.h"
 #include "button.h"
 #include "led_driver.h"
 #include "conversion.h"
+#include "i2cmaster.h"
 
 
 /* define CPU frequency in Mhz here if not defined in Makefile */
@@ -42,8 +44,8 @@ void init(void) {
   adc_init();
   button_init();
   led_init();
-
-  }
+  i2c_init();
+}
 
 int main(void)
 {
@@ -53,11 +55,11 @@ int main(void)
   float temp2;
   float temperature=0;
   enum led_states {
-    LED_OFF = 0,
-    LED_ALL = 1,
-    LED_RED = 2,
-    LED_GREEN = 3,
-    LED_BLUE = 4
+	LED_OFF = 0,
+	LED_ALL = 1,
+	LED_RED = 2,
+	LED_GREEN = 3,
+	LED_BLUE = 4
   };
 
   init();
@@ -71,44 +73,66 @@ int main(void)
   statemachine=LED_OFF;
   led_all_off();
   while(1) {
-    if (is_button0_pressed()) {
-      uart_puts_P("BTN0 pressed.\r\n");
-      DHT22_ERROR_t errorCode = read_dht22( &temp1, &temp2 ); 
-      switch(errorCode) { 
-        case DHT_ERROR_NONE: 
-          uart_puts_P("Temperature: ");
-          dtostrf(temp2, 5, 2, buffer);   // convert interger into string (decimal format)         
-          uart_puts(buffer);        // and transmit string to UART
-          uart_puts_P("\r\nHumidity: ");
-          itoa(temp1, buffer, 10);   // convert interger into string (decimal format)         
-          uart_puts(buffer);        // and transmit string to UART
-          uart_puts_P("\r\n");
-          break; 
-        default:
-          uart_puts_P("Error reading DHT22 sensor.\r\n");
-          break;
-      }
-    }
-    if (is_button1_pressed()) {
-      uart_puts_P("BTN1 pressed.\r\n");
-      temperature=temperature_adc(); // convert from adc value to temperaure 
-      //itoa( temperature, buffer, 10);   // convert interger into string (decimal format)         
-      
-      static char temperature_string_buffer[10];
-      dtostrf(temperature, 9, 4, &temperature_string_buffer);
-      uart_puts_P("Temperature from ADC: ");
-      uart_puts(temperature_string_buffer);        // and transmit string to UART
-      uart_puts_P("\r\n");
-    }
-    if (is_button2_pressed()) {
-      uart_puts_P("BTN2 pressed.\r\n");
-      switch (statemachine) {
-        case LED_OFF: led_all_full(); statemachine = LED_ALL; break;
-        case LED_ALL: led_red_full(); statemachine = LED_RED; break;
-        case LED_RED: led_green_full(); statemachine = LED_GREEN; break;
-        case LED_GREEN: led_blue_full(); statemachine = LED_BLUE; break;
-        case LED_BLUE: led_all_off(); statemachine = LED_OFF; break;
-      }
-    }
+	if (is_button0_pressed()) {
+	  uart_puts_P("BTN0 pressed.\r\n");
+	  uart_puts_P("DHT22 sensor:\r\n");
+	  DHT22_ERROR_t errorCode = read_dht22( &temp1, &temp2 ); 
+	  switch(errorCode) { 
+		case DHT_ERROR_NONE: 
+		  uart_puts_P("Temperature: ");
+		  dtostrf(temp2, 5, 2, buffer);   // convert interger into string (decimal format)         
+		  uart_puts(buffer);        // and transmit string to UART
+		  uart_puts_P("\r\nHumidity: ");
+		  itoa(temp1, buffer, 10);   // convert interger into string (decimal format)         
+		  uart_puts(buffer);        // and transmit string to UART
+		  uart_puts_P("\r\n");
+		  break; 
+		default:
+		  uart_puts_P("Error reading DHT22 sensor.\r\n");
+		  break;
+	  }
+	  uart_puts_P("HYT271 sensor:\r\n");
+	  double hyt271_hum = 0.0;
+	  double hyt271_temp = 0.0;
+	  HYT271_ERROR_t error_code =  hyt271_get_measurements(&hyt271_hum, &hyt271_temp);
+	  switch(error_code) {
+		case HYT271_ERROR_NONE:
+		  uart_puts_P("Temperature: ");
+		  dtostrf(hyt271_temp, 5, 2, buffer);   // convert interger into string (decimal format)         
+		  uart_puts(buffer);        // and transmit string to UART
+		  uart_puts_P("\r\nHumidity: ");
+		  itoa(hyt271_hum, buffer, 10);   // convert interger into string (decimal format)         
+		  uart_puts(buffer);        // and transmit string to UART
+		  uart_puts_P("\r\n");
+		  break;
+		case HYT271_BUS_ERROR:
+		  uart_puts_P("Bus error while reading HYT271 sensor.\r\n");
+		  break;
+		default:
+		  uart_puts_P("Unknown error reading HYT271 sensor.\r\n");
+		  break;
+	  }
+	}
+	if (is_button1_pressed()) {
+	  uart_puts_P("BTN1 pressed.\r\n");
+	  temperature=temperature_adc(); // convert from adc value to temperaure 
+	  //itoa( temperature, buffer, 10);   // convert interger into string (decimal format)         
+
+	  static char temperature_string_buffer[10];
+	  dtostrf(temperature, 9, 4, &temperature_string_buffer);
+	  uart_puts_P("Temperature from ADC: ");
+	  uart_puts(temperature_string_buffer);        // and transmit string to UART
+	  uart_puts_P("\r\n");
+	}
+	if (is_button2_pressed()) {
+	  uart_puts_P("BTN2 pressed.\r\n");
+	  switch (statemachine) {
+		case LED_OFF: led_all_full(); statemachine = LED_ALL; break;
+		case LED_ALL: led_red_full(); statemachine = LED_RED; break;
+		case LED_RED: led_green_full(); statemachine = LED_GREEN; break;
+		case LED_GREEN: led_blue_full(); statemachine = LED_BLUE; break;
+		case LED_BLUE: led_all_off(); statemachine = LED_OFF; break;
+	  }
+	}
   }
 }
