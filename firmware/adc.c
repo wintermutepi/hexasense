679371d30e7356a48c231f9f450b62aad6ec2b01
Mathias Dalheimer <md@gonium.net>
2013-02-06 12:09:05 +0100

    introduced supersampling - currently untested! fixes #29, fixes #30

--- firmware/adc.c
+++ firmware/adc.c
@@ -3,7 +3,8 @@
 // http://www.mikrocontroller.net/articles/AVR-GCC-Tutorial/Analoge_Ein-_und_Ausgabe
 
 void adc_init(void) {
-  // interne Referenzspannung (1.1V) als Referenz für den ADC wählen:
+  // interne Referenzspannung (1.1V) als Referenz für den ADC wählen,
+  // siehe m1284p datasheet S. 255
   ADMUX = (1<<REFS1) | (0<<REFS0);
   
   // Bit ADFR ("free running") in ADCSRA steht beim Einschalten
@@ -14,7 +15,7 @@ void adc_init(void) {
   // 0.05 <= 8/x <= 0.2
   // <=> 0.05*x <= 8 <= 0.2*x
   // Satisfied for x=128. Set Prescaler accordingly:
-  ADCSRA =  (1<<ADPS1) | (1<<ADPS0);     
+  ADCSRA =  (1 << ADPS2) | (1<<ADPS1) | (1<<ADPS0);     
   ADCSRA |= (1<<ADEN);                  // ADC aktivieren
  
   /* nach Aktivieren des ADC wird ein "Dummy-Readout" empfohlen, man liest
@@ -23,8 +24,19 @@ void adc_init(void) {
 }
 
 uint16_t adc_get_single_sample(uint8_t channel) {
+  // Kanal waehlen, ohne andere Bits zu beeinflußen
+  ADMUX = (ADMUX & ~(0x1F)) | (channel & 0x1F);
   ADCSRA |= (1<<ADSC);                  // eine ADC-Wandlung starten
   while (ADCSRA & (1<<ADSC) ) {         // auf Abschluss der Konvertierung warten
   }
   return ADCW;
+}
+
+uint16_t adc_get_super_sample(uint8_t channel) {
+  uint16_t buffer;
+  // no overflow here: 8*1024 <<<< max(uint16_t)
+  for (uint8_t i=0; i < SUPERSAMPLING_MAX_ITERATION; i++) {
+	buffer += adc_get_single_sample(channel);
+  }
+  return buffer/SUPERSAMPLING_MAX_ITERATION;
 }
